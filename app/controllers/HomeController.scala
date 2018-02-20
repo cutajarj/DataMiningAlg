6f73e962bb36937f2a89a4dc807a3a2021b4d7a7
cutajarj2 <james.cutajar@blip.pt>
2018-02-20 16:11:25 +0000

    Added d3 libs and index page to capture points

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -21,4 +21,6 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
+
 }
